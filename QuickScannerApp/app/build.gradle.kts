5de4e2c8bfa4d681ba5af11c23cd1880d3653381
crystal11111 <crystal031230@gmail.com>
2024-03-08 05:15:14 -0700

    generate qr code

--- QuickScannerApp/app/build.gradle.kts
+++ QuickScannerApp/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("com.ncorti:slidetoact:0.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
