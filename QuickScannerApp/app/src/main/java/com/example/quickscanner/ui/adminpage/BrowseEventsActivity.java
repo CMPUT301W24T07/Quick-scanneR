29b7610f0abaca0d25bfd5122fc0965c1edd2bfd
IIIAyaan <AyaanPathan2080@gmail.com>
2024-03-08 07:54:32 -0700

    Browse events list works from admins UI now

--- QuickScannerApp/app/src/main/java/com/example/quickscanner/ui/adminpage/BrowseEventsActivity.java
+++ QuickScannerApp/app/src/main/java/com/example/quickscanner/ui/adminpage/BrowseEventsActivity.java
@@ -1,16 +1,76 @@
 package com.example.quickscanner.ui.adminpage;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.quickscanner.R;
+import com.example.quickscanner.model.Event;
+import com.example.quickscanner.ui.homepage_event.EventArrayAdapter;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class BrowseEventsActivity extends AppCompatActivity {
+
+    // EventList References
+    ListView eventListView;
+    ArrayList<Event> eventsDataList;
+    ArrayAdapter<Event> eventAdapter;
+
+    // Firestore References
+    private FirebaseFirestore db;
+    private CollectionReference eventsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_events);
+
+        // Firebase references
+        db = FirebaseFirestore.getInstance(); // non-image db references
+        eventsRef = db.collection("Events");
+
+        // Store view references
+        eventListView = findViewById(R.id.BrowseEventsListView); 
+
+        // Initialize the event data list and ArrayAdapter
+        eventsDataList = new ArrayList<Event>();
+        eventAdapter = new EventArrayAdapter(this, eventsDataList);
+        // Set the adapter to the ListView
+        eventListView.setAdapter(eventAdapter);
+
+        // Create FireStore Listener for Updates to the Events List.
+        eventsRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot querySnapshots,
+                                @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.e("Firestore", error.toString());
+                    return;
+                }
+                if (querySnapshots != null) {
+                    eventsDataList.clear();  // removes current data
+                    for (QueryDocumentSnapshot doc : querySnapshots) { // set of documents
+                        Event qryEvent = doc.toObject(Event.class);
+
+                        //associate event ID with the retrieved event
+                        qryEvent.setEventID(doc.getId());
+
+                        eventsDataList.add((qryEvent)); // adds new data from db
+                    }
+                }
+                eventAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
