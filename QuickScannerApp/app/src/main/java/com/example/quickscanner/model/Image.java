368670e0b7abd1972d8bef0bb9d3f63347a7efd7
Ayaan <65634439+IIIAyaan@users.noreply.github.com>
2024-04-06 13:56:13 -0600

    Browse Images checkbox deletion implemented, but is a little buggy

--- QuickScannerApp/app/src/main/java/com/example/quickscanner/model/Image.java
+++ QuickScannerApp/app/src/main/java/com/example/quickscanner/model/Image.java
@@ -3,6 +3,8 @@ package com.example.quickscanner.model;
 public class Image {
     private String imageUrl;
 
+    public boolean isSelected;
+
     public Image() {
     }
 
@@ -17,4 +19,8 @@ public class Image {
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
+    public boolean isSelected() { return isSelected; }
+
+    public void setSelected(boolean selected) { isSelected = selected; }
 }
